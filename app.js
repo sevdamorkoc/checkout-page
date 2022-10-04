a85041a4a025fa9ac4cc775ed683ee8baba39c31
Sevda Morkoç <morkocsevdam@gmail.com>
2022-10-04 23:34:38 +0300

    add ürün fiyatı hesap

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ const productsDiv = document.querySelector(".products")
  productsDiv.addEventListener("click", (event) =>{
    // console.log(event);
    if(event.target.className == "fa-solid fa-minus") {
-    console.log("eksi butonu çalışıyor");
+    // console.log("eksi butonu çalışıyor");
     event.target.parentElement.querySelector(".quantity").innerText--;{
         if(event.target.parentElement.querySelector(".quantity").innerText>1){
         event.target.parentElement.querySelector(".quantity").innerText--;
+        ürünFiyatıHesap(event.target)
         }else {
             alert("artık silme");
         }
@@ -32,11 +33,21 @@ const productsDiv = document.querySelector(".products")
     
 }
 else if(event.target.className == "fa-solid fa-plus") {
-    console.log("artı butonu çalışıyor");
+    // console.log("artı butonu çalışıyor");
     //console.log(event.target.previousElementSibling.innerText)
     event.target.previousElementSibling.innerText++;
+    ürünFiyatıHesap(event.target)
 }
 else if (event.target.classList.contains("remove-product")){
-    console.log("remove btn basıldı");
+    // console.log("remove btn basıldı");
+    event.target.closest(".product").remove()
 }
- })
+ });
+ const ürünFiyatıHesap = (button) => {
+    const productInfoDiv = button.parentElement.parentElement
+    // console.log()
+    const ürünFiyatı =  productInfoDiv.querySelector(".product-price strong").innerText;
+    const adet = productInfoDiv.querySelector(".quantity").innerText;
+    const toplamÜrünFiyatı = productInfoDiv.querySelector(".product-line-price");
+    toplamÜrünFiyatı.innerText = (ürünFiyatı * adet)
+  };
